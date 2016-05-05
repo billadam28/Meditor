22086aa14fe9c2f30c3c4edba807930b119102e6
Thodoris21 <tefstathiou@athtech.gr>
2016-05-05 16:25:23 +0300

    script enhancement

--- web/js/validateform.js
+++ web/js/validateform.js
@@ -1,19 +1,19 @@
 function validateForm() {
     var x = document.getElementById("name").value;
     var y = document.getElementById("desc").value;
-    if ((x == null || x == "" || x==" ") && (y==null || y=="" || y==" ")) {
+    if ((x == null || x == "" || x.match(/^\s*$/)) && (y==null || y=="" || y.match(/^\s*$/))) {
         document.getElementById("name").style.borderColor="red";
         document.getElementById("desc").style.borderColor="red";
         var m = document.getElementById("msg").value;
         document.getElementById("msg2").innerHTML = m;
         //alert("All fields are required!");
         return false;
-    } else if (x==null || x=="" || x==" ") {
+    } else if (x==null || x=="" || x.match(/^\s*$/)) {
         document.getElementById("name").style.borderColor="red";
         var m = document.getElementById("msg").value;
         document.getElementById("msg2").innerHTML = m;
         return false;
-    } else if (y==null || y=="" || y==" ") {
+    } else if (y==null || y=="" || y.match(/^\s*$/)) {
         document.getElementById("desc").style.borderColor="red";
         var m = document.getElementById("msg").value;
         document.getElementById("msg2").innerHTML = m;
