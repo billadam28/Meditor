6ba4514d937ab3aa31519f460dfb2cd89c290b9b
Thodoris21 <tefstathiou@athtech.gr>
2016-05-05 15:52:52 +0300

    script enhancement

--- web/js/validateform.js
+++ web/js/validateform.js
@@ -1,19 +1,19 @@
 function validateForm() {
     var x = document.getElementById("name").value;
     var y = document.getElementById("desc").value;
-    if ((x == null || x == "") && (y==null || y=="")) {
+    if ((x == null || x == "" || x==" ") && (y==null || y=="" || y==" ")) {
         document.getElementById("name").style.borderColor="red";
         document.getElementById("desc").style.borderColor="red";
         var m = document.getElementById("msg").value;
         document.getElementById("msg2").innerHTML = m;
         //alert("All fields are required!");
         return false;
-    } else if (x==null || x=="") {
+    } else if (x==null || x=="" || x==" ") {
         document.getElementById("name").style.borderColor="red";
         var m = document.getElementById("msg").value;
         document.getElementById("msg2").innerHTML = m;
         return false;
-    } else if (y==null || y=="") {
+    } else if (y==null || y=="" || y==" ") {
         document.getElementById("desc").style.borderColor="red";
         var m = document.getElementById("msg").value;
         document.getElementById("msg2").innerHTML = m;
